18a82479abd1bc516b2c149592e75b835cc61c17
Nils-Tenelsen-fhw <49761628+Nils-Tenelsen-fhw@users.noreply.github.com>
2022-08-02 15:19:20 +0200

    Added counter for merged file

--- merge_csv.py
+++ merge_csv.py
@@ -4,11 +4,14 @@ files = ['phishing0.csv', 'phishing1.csv', 'phishing2.csv', 'phishing3.csv',
          '20051114.csv',
          'phishing-2015.csv', 'phishing-2016.csv', 'phishing-2017.csv', 'phishing-2018.csv', 'phishing-2019.csv', 'phishing-2020.csv', 'phishing-2021.csv',
          'private-phishing4.csv' ]
-
+counter = 0
 with open('all_filtered.csv', 'w', newline='\n') as outputfile:
     csvwriter = csv.writer(outputfile, dialect='excel')
     for file in files:
         with open('output_files/' + file, 'r', newline='\n') as csvfile:
             csvreader = csv.reader(csvfile, dialect='excel')
             for row in csvreader:
+                counter += 1
                 csvwriter.writerow(row)
+
+print('Total mails: ' + str(counter))
